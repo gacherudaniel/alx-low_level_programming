4fde9846345c9bc4b5cca29b8bc2ceba420c20e9
gacherudaniel <danielgacheru01@gmail.com>
2023-03-16 09:12:53 -0700

    0. Preprocessor

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	const char str[] = "and that piece of art is useful\" - "
 		"Dora Korpar, 2015-10-19\n";
-
+	
 	const int len = sizeof(str) - 1;
 
 	write(STDERR_FILENO, str, len);
